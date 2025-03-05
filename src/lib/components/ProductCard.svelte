e5d4ecfb1981b8c6254a0246f2375f848147546d
chanios <chanios@proton.me>
2025-03-05 20:08:27 +0700

    update

--- src/lib/components/ProductCard.svelte
+++ src/lib/components/ProductCard.svelte
@@ -1,30 +1,42 @@
 <script>
     import { Package2 } from "lucide-svelte";
+
+    let { product } = $props();
 </script>
 
 <a class="border rounded-lg group" href="#">
     <div
-        class="bg-background h-36 w-full flex items-center justify-center text-sky-500 relative"
+        class="bg-background h-40 w-full flex items-center justify-center text-sky-500 relative overflow-hidden"
     >
         <div
             class="absolute top-2 right-3 bg-accent/50 transition-colors group-hover:bg-accent px-2 rounded-lg text-primary"
         >
-            รหัส
+            {product.category}
         </div>
-        <Package2 class="w-10 h-10 group-hover:scale-125 transition-all" />
+        {#if product.image}
+            <img
+                class="w-fit h-fit object-cover group-hover:scale-125 transition-all"
+                src={product.image}
+                alt=""
+            />
+        {:else}
+            <Package2 class="w-10 h-10 group-hover:scale-125  transition-all" />
+        {/if}
     </div>
     <div
         class="p-2 flex gap-3 justify-between flex-col bg-accent/50 transition-colors group-hover:bg-accent"
     >
-        <div>หีเด็ก</div>
+        <div>{product.name}</div>
         <div class="flex justify-between">
             <div>
-                <span class="text-sky-500 font-bold text-md">50.00 บาท</span>
+                <span class="text-sky-500 font-bold text-md"
+                    >{product.price.toFixed(2)} บาท</span
+                >
             </div>
 
             <div>
                 สต็อก
-                <span class="text-green-500 text-md">254+</span>
+                <span class="text-green-500 text-md">{product.stock}+</span>
             </div>
         </div>
     </div>
