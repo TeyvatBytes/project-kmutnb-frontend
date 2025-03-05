3fdc45bbb9f89960afbff9401074b762d279c673
chanios <chanios@proton.me>
2025-03-06 00:49:33 +0700

    update

--- src/lib/components/Copyright.svelte
+++ src/lib/components/Copyright.svelte
@@ -1,9 +1,7 @@
-<div
-    class="mt-8 pt-6 border-t border-gray-200 text-xs text-gray-500 text-center"
->
+<div class="mt-8 py-6 border-gray-200 text-xs text-gray-500 text-center">
     <p>
         © 2025 <a
-            class=" border-b-2 border-dotted"
+            class=" border-b-2 border-dotted hover:text-primary transition-colors"
             href="https://teyvatbytes.com">TeyvatBytes Inc.</a
         > สงวนลิขสิทธิ์ทั้งหมด.
     </p>
